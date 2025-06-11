4c41c9aaddcd6db970ee79db85a0134163aa017f
Kevin Koch <kevkoch1@googlemail.com>
2025-06-11 20:50:03 +0200

    fix ios

---
+++ lib/assets_audio_player.dart
@@ -0,0 +1,13 @@
+library assets_audio_player;
+
+export 'package:assets_audio_player_plus/src/assets_audio_player.dart';
+export 'package:assets_audio_player_plus/src/player_group.dart';
+export 'package:assets_audio_player_plus/src/playable.dart';
+export 'package:assets_audio_player_plus/src/playing.dart';
+export 'package:assets_audio_player_plus/src/builders/player_builders.dart';
+export 'package:assets_audio_player_plus/src/builders/player_builders_ext.dart';
+export 'package:assets_audio_player_plus/src/widget/audio_widget.dart';
+export 'package:assets_audio_player_plus/src/notification.dart';
+export 'package:assets_audio_player_plus/src/loop.dart';
+export 'package:assets_audio_player_plus/src/cache/cache.dart';
+export 'package:assets_audio_player_plus/src/errors.dart';
